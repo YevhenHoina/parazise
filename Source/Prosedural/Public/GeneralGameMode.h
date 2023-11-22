00250a14784931424043ad924cfde553a44b6559
YevhenHoina <yevhenhoina@gmail.com>
2023-11-22 16:04:38 +0200

    initialized cpp-classes

---
+++ Source/Prosedural/Public/GeneralGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "GeneralGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROSEDURAL_API AGeneralGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/Prosedural/Public/PlayerCharacterController.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+//#include "EnhancedInputSubsystems.h"
+#include "PlayerCharacterController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROSEDURAL_API APlayerCharacterController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
